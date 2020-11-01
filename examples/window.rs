cab84c4462f588df373becc1538bc29e35027fcd
Interfiber <stasonis@gmail.com>
2020-11-01 10:35:52 -0500

    :sparkles: Allow User to assign window height and width

--- examples/window.rs
+++ examples/window.rs
@@ -3,8 +3,8 @@ use maple::*;
 fn main(){
 	let win = Window {
 		title: String::from("My Awsome Window!"),
-		height: 14,
-		width: 14,
+		height: 900,
+		width: 900,
 		name: String::from("Maple Test")
 	};
 	win.start_window();
--- src/lib.rs
+++ src/lib.rs
@@ -7,20 +7,26 @@ use glfw::{Action, Context, Key};
 
 pub struct Window {
 	pub title: String,
-	pub height: i32,
-	pub width: i32,
+	pub height: u32,
+	pub width: u32,
 	pub name: String
 }
 impl Window {
 	pub fn get_title(&self) -> String{
 		return self.title.to_string()
 	}
+	pub fn get_width(&self) -> u32 {
+		return self.width
+	}
+	pub fn get_height(&self) -> u32 {
+		return self.height;
+	}
 	pub fn start_window(&self){
 		println!("Starting SimpleLogger...");
 	    SimpleLogger::new().init().unwrap();
     	info!("Creating Window...");
     	let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-		let (mut window, events) = glfw.create_window(300, 300, &self.get_title().to_string(), glfw::WindowMode::Windowed)
+		let (mut window, events) = glfw.create_window(self.get_width(), self.get_height(), &self.get_title().to_string(), glfw::WindowMode::Windowed)
 		     .expect("Failed to create GLFW window.");
 		window.set_key_polling(true);
 		window.make_current();
