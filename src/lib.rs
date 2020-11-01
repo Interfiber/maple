7245c483700162d6de572275051b5ea44e42affb
Interfiber <stasonis@gmail.com>
2020-11-01 13:36:32 -0500

    :sparkles: Adds load function support

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 use simple_logger::SimpleLogger;
-use std::thread;
 use log::*;
 extern crate glfw;
 
-use glfw::{Action, Context, Key};
-
+use glfw::{Context};
+static mut IS_OPEN: bool = false;
 pub struct Window {
 	pub title: String,
 	pub height: u32,
@@ -21,16 +20,32 @@ impl Window {
 	pub fn get_height(&self) -> u32 {
 		return self.height;
 	}
-	pub fn start_window(&self){
+	pub fn is_open(&self) -> bool{
+		unsafe {
+			if !IS_OPEN {
+				return false
+			}else {
+				return true
+			}
+		}
+	}
+	pub fn start_window(&self, load_func: fn(String) -> String){
 		println!("Starting SimpleLogger...");
 	    SimpleLogger::new().init().unwrap();
     	info!("Creating Window...");
     	let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-		let (mut window, events) = glfw.create_window(self.get_width(), self.get_height(), &self.get_title().to_string(), glfw::WindowMode::Windowed)
+		let (mut window, _events) = glfw.create_window(self.get_width(), self.get_height(), &self.get_title().to_string(), glfw::WindowMode::Windowed)
 		     .expect("Failed to create GLFW window.");
 		window.set_key_polling(true);
 		window.make_current();
 		info!("Window Created!");
+		info!("Running load_func code on other thread...");
+		std::thread::spawn(move||{
+			load_func("Start".to_string());
+		});
+		unsafe {
+			IS_OPEN = true;
+		}
 		while !window.should_close() {
 		    glfw.poll_events();	   
  	  }
