b6f96ead4ae33f1df5e470f1b79f534bec1fdfdd
Interfiber <stasonis@gmail.com>
2020-11-02 08:07:08 -0500

    :sparkles: Allows user to set the cursor type for the window

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,14 @@ use log::*;
 extern crate glfw;
 
 use glfw::{Context};
+
+use image::imageops::{resize, Nearest};
+use image::{open as open_image, DynamicImage};
+// Statics
 static mut IS_OPEN: bool = false;
+static mut HAND_CURSOR: bool = false;
+static mut CROSSHAIR_CURSOR: bool = false;
+static mut DEFAULT_CURSOR: bool = false;
 pub struct Window {
 	pub title: String,
 	pub height: u32,
@@ -29,6 +36,17 @@ impl Window {
 			}
 		}
 	}
+	pub fn set_cursor_type(&self, cursor: &str){
+		unsafe {
+			if cursor == "hand" {
+				HAND_CURSOR = true;
+			}else if cursor == "default" {
+				DEFAULT_CURSOR = true;
+			}else if cursor == "crosshair" {
+				CROSSHAIR_CURSOR = true;
+			}
+		}
+	}
 	pub fn start_window(&self, load_func: fn(String) -> String){
 		println!("Starting SimpleLogger...");
 	    SimpleLogger::new().init().unwrap();
@@ -39,15 +57,32 @@ impl Window {
 		window.set_key_polling(true);
 		window.make_current();
 		info!("Window Created!");
+		info!("Checking for cursor type...");
+		unsafe {
+			if CROSSHAIR_CURSOR {
+				info!("Setting Cursor type to crosshair...");
+				let crosshair_cursor = glfw::Cursor::standard(glfw::StandardCursor::Crosshair);
+				window.set_cursor(Some(crosshair_cursor));
+			}else if HAND_CURSOR {
+				info!("Setting Cursor type to hand...");
+				let hand_cursor = glfw::Cursor::standard(glfw::StandardCursor::Hand);
+				window.set_cursor(Some(hand_cursor));
+			}else if DEFAULT_CURSOR{
+				info!("Setting Cursor type to default...");
+				let arrow_cursor = glfw::Cursor::standard(glfw::StandardCursor::Arrow);
+				window.set_cursor(Some(arrow_cursor));
+			}
+		}
 		info!("Running load_func code on other thread...");
 		std::thread::spawn(move||{
 			load_func("Start".to_string());
 		});
+
 		unsafe {
 			IS_OPEN = true;
 		}
 		while !window.should_close() {
-		    glfw.poll_events();	   
+		    glfw.poll_events();
  	  }
   }
 }
